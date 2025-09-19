651603ce1c9e6818900a0d08bfaa6ba29e9088a0
arifarman02 <arifarman1820@gmail.com>
2025-09-19 19:39:43 +0100

    Cleaned data and added to a csv file

--- data/combine_csv.py
+++ data/combine_csv.py
@@ -11,4 +11,25 @@ for csv_file in csv_files:
 
 combined_csv = pd.concat(data_frames, ignore_index=True)
 
+# Filter for pink morsel only
+combined_csv = combined_csv[combined_csv['product'] == 'pink morsel']
+
+# Remove the $ sign from price
+combined_csv['price'] = combined_csv['price'].str.replace('$', '')
+
+print(combined_csv.dtypes)
+
+# Convert price to float
+combined_csv['price'] = combined_csv['price'].astype(float)
+combined_csv['sales'] = combined_csv['price'] * combined_csv['quantity']
+
+# Drop the price and quantity columns
+combined_csv = combined_csv.drop(['product', 'price', 'quantity'], axis=1)
+
+
+# Rearrange the position of columns
+combined_csv = combined_csv.iloc[:, [2, 0, 1]]
+print(combined_csv.head())
+
+# Save cleaned data to a csv file
 combined_csv.to_csv('sales.csv', index=False)

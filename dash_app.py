cb33178c4c7cf5f77b57606c73577143ec67e4f6
arifarman02 <arifarman1820@gmail.com>
2025-09-22 13:23:41 +0100

    Added a line graph WIP

--- dash_app.py
+++ dash_app.py
@@ -1,5 +1,21 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-app = Dash()
+df = pd.read_csv('./data/sales.csv')
+
+app = Dash(__name__)
+
+fig = px.line(df, x='date', y='sales', title='pink morsel sales')
+
+app.layout = html.Div(children=[
+    html.H1(children='pink morsel sales'),
+    dcc.Graph(
+        id='line-chart',
+        figure=fig
+    )
+]),
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

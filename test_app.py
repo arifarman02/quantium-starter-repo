b7dbb4ae6d173896c923bb7a1a56e03ec5bf8d3b
arifarman02 <arifarman1820@gmail.com>
2025-09-25 15:28:00 +0100

    Inluded test scripts, all passed

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html
+from dash import html, dcc
 
 def test_present_header(dash_duo):
     app = dash.Dash(__name__)
@@ -7,3 +7,30 @@ def test_present_header(dash_duo):
     dash_duo.start_server(app)
     header_element = dash_duo.find_element('h1')
     assert header_element.text == 'Pink Morsel Sales'
+
+def test_visualisation(dash_duo):
+    app = dash.Dash(__name__)
+    app.layout = dcc.Graph(id='graph-output', style={'padding': '20px'})
+    dash_duo.start_server(app)
+    graph_element = dash_duo.find_element('#graph-output')
+    assert graph_element is not None
+
+def test_region_picker(dash_duo):
+    colors = {
+    'background': '#FACBAA',
+    'text': '#FFFFFF'
+    }
+    app = dash.Dash(__name__)
+    app.layout = dcc.RadioItems(
+            ['north', 'south', 'east', 'west', 'all'],
+            'all',
+            id='radio-items-input',
+            inline=True,
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        )
+    dash_duo.start_server(app)
+    radio_items_element = dash_duo.find_element('#radio-items-input')
+    assert radio_items_element is not None

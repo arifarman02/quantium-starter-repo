d9941b4b9700105d94ef7cb0f4268ccb1287ec95
arifarman02 <arifarman1820@gmail.com>
2025-09-25 15:34:34 +0100

    Previous push was wrong

--- test_app.py
+++ test_app.py
@@ -1,36 +1,19 @@
 import dash
 from dash import html, dcc
+import pytest
+from app import app as dash_app
 
 def test_present_header(dash_duo):
-    app = dash.Dash(__name__)
-    app.layout = html.H1('Pink Morsel Sales')
-    dash_duo.start_server(app)
-    header_element = dash_duo.find_element('h1')
+    dash_duo.start_server(dash_app)
+    header_element = dash_duo.wait_for_element('h1', timeout=10)
     assert header_element.text == 'Pink Morsel Sales'
 
 def test_visualisation(dash_duo):
-    app = dash.Dash(__name__)
-    app.layout = dcc.Graph(id='graph-output', style={'padding': '20px'})
-    dash_duo.start_server(app)
-    graph_element = dash_duo.find_element('#graph-output')
+    dash_duo.start_server(dash_app)
+    graph_element = dash_duo.wait_for_element('#graph-output', timeout=10)
     assert graph_element is not None
 
 def test_region_picker(dash_duo):
-    colors = {
-    'background': '#FACBAA',
-    'text': '#FFFFFF'
-    }
-    app = dash.Dash(__name__)
-    app.layout = dcc.RadioItems(
-            ['north', 'south', 'east', 'west', 'all'],
-            'all',
-            id='radio-items-input',
-            inline=True,
-            style={
-                'textAlign': 'center',
-                'color': colors['text']
-            }
-        )
-    dash_duo.start_server(app)
-    radio_items_element = dash_duo.find_element('#radio-items-input')
+    dash_duo.start_server(dash_app)
+    radio_items_element = dash_duo.wait_for_element('#radio-items-input', timeout=10)
     assert radio_items_element is not None
